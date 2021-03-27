8b6e94d24d976f82a30127e98b87296b9e13c850
Aleksandar <41162669+Aca84@users.noreply.github.com>
2021-03-27 18:49:58 +0100

    Update
    Admin panel creation

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+    public function index()
+    {
+        if (Auth::user()->role('admin')) {
+
+            return view('admin.index');
+        }
+
+        return redirect('posts');
+    }
+
+}
